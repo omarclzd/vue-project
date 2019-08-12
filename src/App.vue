3bc4d7a0336fd7015a71fe99f0c4b2642d62208b
omar <omasrt@gmail.com>
2019-08-12 15:13:10 -0500

    adds footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <v-app id="app">
     <app-navigation></app-navigation>
-    <router-view />
+    <v-content>
+      <router-view />
+    </v-content>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import AppNavigation from "@/components/AppNavigation";
+import AppFooter from "@/components/AppFooter";
 
 export default {
   name: "App",
   components: {
-    AppNavigation
-  },
-  data: () => ({
-    //
-  })
+    AppNavigation,
+    AppFooter
+  }
 };
 </script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer height="auto" color="indigo">
+    <v-layout row wrap justify-center>
+      <v-btn v-for="link in links" :key="link" color="white" flat>{{ link }}</v-btn>
+      <v-flex xs12 py-3 text-xs-center white--text>
+        &copy;2018 —
+        <strong class="text-center">Vuetify</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
